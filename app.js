785a2e16849dfd791d019ca8832a26c7e8351d16
Shannon <shannon.nell92@gmail.com>
2021-06-03 13:51:10 -0400

    Gitignore and name question from inquirer generated

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
-console.log(profileDataArgs);
+inquirer
+  //recieve array of object in it's argument; known as the question object
+  .prompt([
+    {
+      type: 'input',
+      name: 'name', 
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const [Name, github] = profileDataArgs;
-console.log(Name, github);
+// const pageHTML = generatePage(Name, github);
 
-const pageHTML = generatePage(Name, github);
+// //fs.writeFile(name of file being created, data that will be written on file, callback function for error handling)
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-//fs.writeFile(name of file being created, data that will be written on file, callback function for error handling)
-fs.writeFile('index.html', generatePage(Name, github), err => {
-  if (err) throw err;
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
