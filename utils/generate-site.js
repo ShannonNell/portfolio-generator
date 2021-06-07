5ff908091f7fc8e19c3de115fe1950b838527d09
Shannon <shannon.nell92@gmail.com>
2021-06-07 10:17:44 -0400

    write file using promises

---
+++ utils/generate-site.js
@@ -0,0 +1,35 @@
+//import fs into file
+const fs = require('fs');
+
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/index.html', fileContent, err => {
+            //if error, reject promise, send error to Promise's `catch()` method
+            if (err) {
+                reject(err);
+                //return out of function to make sure promise doesn't accidentally resolve
+                return;
+            }
+            //if everything went well, resolve promise and send to then() method
+            resolve ({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    });
+};
+
+const copyFile = () => {
+    return new Promise((resolve, reject) => {
+        fs.copyFile('./src/style.css', './dist/style.css', err => {
+            if (err) {
+              reject(err);
+              return;
+            }
+            resolve({
+                ok: true,
+                message: 'Style sheet copied successfully!'
+            });
+        });
+    });
+};
